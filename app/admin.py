241d572c3617260fb7f0acc4d4b2ff0e855119e3
arshadul <arshadul.alam778@gmail.com>
2024-02-26 08:33:13 +0600

    t

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.React)
+class ReactAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'department']
--- app/models.py
+++ app/models.py
@@ -6,3 +6,9 @@ from django.db import models
 class React(models.Model):
     employee = models.CharField(max_length=255)
     department = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.employee
+
+    class Meta:
+        ordering = ['employee']
